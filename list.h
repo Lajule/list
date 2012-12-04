10bacef46f207c89ed56f6f005f71d29b1548e1e
Lajule <julien.rouzieres@mac.com>
2012-12-04 13:05:16 +0100

    Update comments in sources

--- list.h
+++ list.h
@@ -8,55 +8,55 @@ typedef struct list_s {
 	struct list_s *next;
 } list_t;
 
-/* Add a node before the first one, return the new head of list. */
+/* Adds a node before the first one, returns the new head of list. */
 list_t *l_push(list_t *, list_t *);
 
-/* Call the given function (if non-zero) on the first node before destroy it,
- * return the new head of list. */
+/* Calls the given function (if non-zero) on the first node before destroy it,
+ * returns the new head of list. */
 list_t *l_pop(list_t *, void (*)(list_t *));
 
-/* Insert a node at the given position, return the head of list. */
+/* Inserts a node at the given position, returns the head of list. */
 list_t *l_insert(list_t *, size_t, list_t *);
 
-/* Call the given function (if non-zero) on the node at the given position
- * before destroy it, return the head of list. */
+/* Calls the given function (if non-zero) on the node at the given position
+ * before destroy it, returns the head of list. */
 list_t *l_erase(list_t *, size_t, void (*)(list_t *));
 
-/* Call the given function (if non-zero) on each node before destroy them,
- * return zero. */
+/* Calls the given function (if non-zero) on each node before destroy them,
+ * returns zero. */
 list_t *l_free(list_t *, void (*)(list_t *));
 
-/* Return the number of nodes in list. */
+/* Returns the number of nodes in list. */
 size_t l_size(list_t *);
 
-/* Return the node at the given position. */
+/* Returns the node at the given position. */
 list_t *l_node(list_t *, size_t);
 
-/* Search a node in list using the given three-way comparison function. */
+/* Searches a node in list using the given three-way comparison function. */
 list_t *l_find(list_t *, list_t *, int (*)(const list_t *, const list_t *));
 
-/* Walk through list since the given function return zero. */
+/* Walks through list since the given function returns zero. */
 list_t *l_iter(list_t *, int (*)(list_t *));
 
-/* Reverse a list, return the new head of list. */
+/* Reverses a list, returns the new head of list. */
 list_t *l_rev(list_t *);
 
-/* Append two lists. */
+/* Appends two lists. */
 list_t *l_cat(list_t *, list_t *);
 
-/* Insert a node at the position selected by the given three-way comparison
+/* Inserts a node at the position selected by the given three-way comparison
  * function. */
 list_t *l_insert_if(list_t *, list_t *,
 	int (*)(const list_t *, const list_t *));
 
-/* Call the given function (if non-zero) on the node at the position selected
- * by the given three-way comparison function before destroy it, return
+/* Calls the given function (if non-zero) on the node at the position selected
+ * by the given three-way comparison function before destroy it, returns
  * the head of list. */
 list_t *l_erase_if(list_t *, list_t *,
 	int (*)(const list_t *, const list_t *), void (*)(list_t *));
 
-/* Sort (merge algorithm) the list using the given three-way comparison
- * function, return the head of list. */
+/* Sorts (merge algorithm) the list using the given three-way comparison
+ * function, returns the head of list. */
 list_t *l_sort(list_t *, int (*)(const list_t *, const list_t *));
 
 #endif
--- tree.h
+++ tree.h
@@ -9,34 +9,34 @@ typedef struct tree_s {
 	struct tree_s *right;
 } tree_t;
 
-/* Add a node in tree using the given three-way comparison function. */
+/* Adds a node in tree using the given three-way comparison function. */
 tree_t *t_put(tree_t *, tree_t *, int (*)(const tree_t *, const tree_t *));
 
-/* Call the given function (if non-zero) on the node selected using the given
+/* Calls the given function (if non-zero) on the node selected using the given
  * three-way comparison function before destroy it. */
 tree_t *t_erase(tree_t *, tree_t *, int (*)(const tree_t *, const tree_t *),
 		void (*)(tree_t *));
 
-/* Call the given function (if non-zero) on each node before destroy them,
- * return zero. */
+/* Calls the given function (if non-zero) on each node before destroy them,
+ * returns zero. */
 tree_t *t_free(tree_t *, void (*)(tree_t *));
 
-/* Search a node in tree using the given three-way comparison function. */
+/* Searches a node in tree using the given three-way comparison function. */
 tree_t *t_find(tree_t *, tree_t *, int (*)(const tree_t *, const tree_t *));
 
-/* Balance a tree, return the new root node of tree. */
+/* Balances a tree, returns the new root node of tree. */
 tree_t *t_balance(tree_t *);
 
-/* Return the number of nodes in tree. */
+/* Returns the number of nodes in tree. */
 size_t t_size(tree_t *);
 
-/* Return the depth of tree. */
+/* Returns the depth of tree. */
 size_t t_depth(tree_t *);
 
-/* Return the node at the given position. */
+/* Returns the node at the given position. */
 tree_t *t_node(tree_t *, size_t);
 
-/* Walk through tree since pre-order, in-order or post-order given functions
+/* Walks through tree since pre-order, in-order or post-order given functions
  * (if non-zero) return zero. */
 tree_t *t_iter(tree_t *, int (*)(tree_t *), int (*)(tree_t *),
 		int (*)(tree_t *));
